7ada915986e745beda5b2a950687f923d6137e43
StormchaserTed <ted.strombeck1@gmail.com>
2022-10-03 10:28:18 -0500

    Adjusted close button for undergrad and postgrad side bar pop up

--- scripts/postgrad.js
+++ scripts/postgrad.js
@@ -1,59 +1,62 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
-			function openNav() {
-				var navBarElement = document.getElementById("sideNavBar");
- 	 			navBarElement.style.width = "100px";
- 	 			navBarElement.style.height = "150px";
- 	 			navBarElement.style.backgroundColor = "rgba(0,0,0,0.4)";
-  				document.getElementById("sideNavMain").style.marginLeft = "5px";
-  				navBarElement.innerHTML = "<a href=\"javascript:void(0)\" class=\"closebtn\" onclick=\"closeNav()\" style=\"text-align:right;\">Close</a><br/>\
-        								   <a href=\"javascript:void(0)\" onclick=\"getAboutSection()\">About</a> <br/>\
-        								   <a href=\"javascript:void(0)\" onclick=\"getServicesSection()\">Services</a> <br/>\
-        								   <a href=\"javascript:void(0)\" onclick=\"getStudentPortalSection()\">Student Portal</a> <br/>\
-        								   <a href=\"javascript:void(0)\" onclick=\"getContactSection()\">Contact</a> <br/>";
-			}
+function openNav() {
+	var navBarElement = document.getElementById("sideNavBar");
+	navBarElement.style.width = "100px";
+	navBarElement.style.height = "150px";
+	navBarElement.style.backgroundColor = "rgba(0,0,0,0.4)";
+	document.getElementById("sideNavMain").style.marginLeft = "5px";
+	navBarElement.innerHTML = "<a href=\"javascript:void(0)\" class=\"closebtn\" onclick=\"closeNav()\" style=\"text-align:right;\">x</a><br/>\
+							   <a href=\"javascript:void(0)\" onclick=\"getAboutSection()\">About</a> <br/>\
+							   <a href=\"javascript:void(0)\" onclick=\"getServicesSection()\">Services</a> <br/>\
+							   <a href=\"javascript:void(0)\" onclick=\"getStudentPortalSection()\">Student Portal</a> <br/>\
+							   <a href=\"javascript:void(0)\" onclick=\"getContactSection()\">Contact</a> <br/>";
+}
 
-			/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
-			function closeNav() {
-				var navBarElement = document.getElementById("sideNavBar");
-  				navBarElement.style.width = "0";
-  				navBarElement.style.height = "0";
-  				navBarElement.style.backgroundColor = "white";
-  				document.getElementById("sideNavMain").style.marginLeft = "0";
-  				navBarElement.innerHTML = "";
-  				document.getElementById("sideContent").innerHTML = "";
-			}
-			
-			/* About section html generation */
-			function getAboutSection() {
-				var element = document.getElementById("sideContent");
-				element.innerHTML= "<h5>About us</h5>\
-									<p>Our Mission: To seek individuals who are willing to become informed citizens,\
-									thoughtful stewards, critical thinkers, and responsible leaders.</p>\
-									<p>If you would like to apply click <a href=\"404.html\">here</a></p>\
-									";		
-			}
-			
-			function getServicesSection() {
-				var element = document.getElementById("sideContent");
-				element.innerHTML= "<h5>Services</h5>\
-									<p>If you have any questions or inquires about classes please reach out to: registration@example.com</p>\
-									<p>If you have any questions about classes please reach out to: registration@example.com</p>\
-									<p>Any other post-graduate inquires please reach out to postGradHelp@example.com</p>";	
-			}
-			
-			function getContactSection() {
-				var element = document.getElementById("sideContent");
-				element.innerHTML= "<h5>Contact us</h5>\
-									<p>If you have any questions or inquires about classes please reach out to: registration@example.com</p>\
-									<p>If you have any questions about housing please reach out to: campuslife@example.com</p>\
-									<p>all other requests can be asked at: help@example.com</p>";	
-			}
-			
-			function getStudentPortalSection() {
-				var element = document.getElementById("sideContent");
-				element.innerHTML= "<h5>Student Portal</h5>\
-									<a href=\"404.html\">Postgraduate Email</a><br\>\
-									<a href=\"404.html\">Postgraduate Finances</a><br\>\
-									<a href=\"404.html\">Postgraduate Mentorship</a><br\>\
-									<a href=\"404.html\">Postgraduate Classes</a>";	
-			}
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+	var navBarElement = document.getElementById("sideNavBar");
+	navBarElement.style.width = "0";
+	navBarElement.style.height = "0";
+	navBarElement.style.backgroundColor = "white";
+	document.getElementById("sideNavMain").style.marginLeft = "0";
+	navBarElement.innerHTML = "";
+	document.getElementById("sideContent").innerHTML = "";
+}
+
+/* About section html generation */
+function getAboutSection() {
+	var element = document.getElementById("sideContent");
+	element.innerHTML= "<h5>About us</h5>\
+						<p>Our Mission: To seek individuals who are willing to become informed citizens,\
+						thoughtful stewards, critical thinkers, and responsible leaders.</p>\
+						<p>If you would like to apply click <a href=\"404.html\">here</a></p>\
+						";		
+}
+
+/* Services section html generation */
+function getServicesSection() {
+	var element = document.getElementById("sideContent");
+	element.innerHTML= "<h5>Services</h5>\
+						<p>If you have any questions or inquires about classes please reach out to: registration@example.com</p>\
+						<p>If you have any questions about classes please reach out to: registration@example.com</p>\
+						<p>Any other post-graduate inquires please reach out to postGradHelp@example.com</p>";	
+}
+
+/* Contact section html generation */
+function getContactSection() {
+	var element = document.getElementById("sideContent");
+	element.innerHTML= "<h5>Contact us</h5>\
+						<p>If you have any questions or inquires about classes please reach out to: registration@example.com</p>\
+						<p>If you have any questions about housing please reach out to: campuslife@example.com</p>\
+						<p>all other requests can be asked at: help@example.com</p>";	
+}
+
+/* Student Portal Section html generation */
+function getStudentPortalSection() {
+	var element = document.getElementById("sideContent");
+	element.innerHTML= "<h5>Student Portal</h5>\
+						<a href=\"404.html\">Postgraduate Email</a><br\>\
+						<a href=\"404.html\">Postgraduate Finances</a><br\>\
+						<a href=\"404.html\">Postgraduate Mentorship</a><br\>\
+						<a href=\"404.html\">Postgraduate Classes</a>";	
+}
