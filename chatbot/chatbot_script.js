74ca58051b8ce0393885d54202c9d49243b0e302
StormchaserTed <ted.strombeck1@gmail.com>
2022-10-05 14:07:11 -0500

    Initial chatbot character animation complete

--- chatbot/chatbot_script.js
+++ chatbot/chatbot_script.js
@@ -1,16 +1,20 @@
 var id = null;
 function chatbotMove() {
-  var elem = document.getElementById("chatbotAnimationArea"); 
+  var elem = document.getElementById("chatbotAnimationElement"); 
   var pos = 0;
   clearInterval(id);
   id = setInterval(frame, 10);
   function frame() {
-    if (pos == 350) {
+    if (pos == 100) {
       clearInterval(id);
     } else {
       pos++; 
-      elem.style.top = pos + 'px'; 
-      elem.style.left = pos + 'px'; 
+      elem.style.bottom = pos + 'px';
     }
   }
+  loadDialogOptions();
+}
+
+function loadDialogOptions() {
+	console.log("Dialog options not implemented yet");
 }
