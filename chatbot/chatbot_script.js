510298ce3e6daba9b61992e1f8607cd88458d6f4
StormchaserTed <ted.strombeck1@gmail.com>
2022-10-23 16:44:07 -0500

    Added feeling lucky option to chatbot

--- chatbot/chatbot_script.js
+++ chatbot/chatbot_script.js
@@ -40,13 +40,31 @@ function loadDialogOptions() {
 	var elem = document.getElementById("chatbotSelectionItems");
 	
 	// PUT YOUR OPTIONS TO BE ADEED IN THE DROP DOWN MENU BELOW
-	elem.innerHTML = "";
+	elem.innerHTML = "<h3>Hi, how can I help you?</h3>\
+					  <a class=\"nav-item nav-link\" href=\"post-grad.html\">General Post-Graduates</a>\
+					  <a class=\"nav-item nav-link\" href=\"under-grad.html\">General Under-Graduates</a>\
+					  <a class=\"nav-item nav-link\" href=\"academic_advising.html\">General Academic Advising</a>\
+					  <a class=\"nav-item nav-link\" onclick=\"feelingLucky()\">I'm feeling lucky!</a>\
+					  <a class=\"nav-item nav-link\" href=\"index.html\">If none of those helped... you could try our AI chatbot?</a>\
+					  ";
+}
+
+function removeDialogOptions() {
+
+}
+
+function feelingLucky() {
+	pages = ["index.html", "post-grad.html", "under-grad.html", "academic_advising.html"];
+	
+	var num = Math.round(Math.random() * 3); // increase the number to reflect the (number of pages) - 1
+	
+	document.location.href=pages[num], true; // changes the url to a random web page
 }
 
 function changeChatbotImage(filepath, height, width, option) {
 	var elem = document.getElementById("chatbotAnimationElement");
 	elem.innerHTML= "<img src=\"" + filepath + "\" height=\"" + height + "px\" \
 	width=\"" + width + "px\" onclick=\"chatbotMove" + option + "()\"></img>";
-	console.log(elem.innerHTML);
-	console.log("Chatbot Image has been updated"); // TODO: Remove
+	//console.log(elem.innerHTML);
+	//console.log("Chatbot Image has been updated"); // TODO: Remove
 }
